21abff409f993c44de5822d6df4c2b3aef7b7437
amirdi <amir.dirin@yahoo.com>
2024-08-26 21:56:00 +0300

    third commit, main changed

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        int a = 5;
+        int b = 6;
+        int c = a + b;
+        System.out.println("here is the result: "+c);
     }
 }
